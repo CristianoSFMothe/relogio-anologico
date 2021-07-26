472c65f8f1ec25349a68fcb5220f11f98ae2afb6
CRISTIANO DA SILVA FERREIRA <68359459+CristianoDaSilvaFerreira@users.noreply.github.com>
2021-07-26 18:53:09 -0300

    :up: ponteiro da hora posicionando corretamente

--- js/script.js
+++ js/script.js
@@ -13,9 +13,11 @@ function updateClock() {
 
   let sDeg = ((360 / 60) * second) - 90;
   let mDeg = ((360 / 60) * minute) - 90;
+  let hDeg = ((360 / 12) * hour) - 90;
 
   sElement.style.transform = `rotate(${sDeg}deg)`;
   mElement.style.transform = `rotate(${mDeg}deg)`;
+  hElement.style.transform = `rotate(${hDeg}deg)`;
 }
 
 function fixZero(time) {
